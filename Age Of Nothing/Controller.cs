3629dace09e5947ace7e1d1ae3c56dafcbcc952c
Lpi <thelpi.v3@gmail.com>
2023-03-06 16:48:53 +0100

    controller abstraction

---
+++ Age Of Nothing/Controller.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Age_Of_Nothing.Sprites;
+
+namespace Age_Of_Nothing
+{
+    public class Controller
+    {
+        private readonly List<Unit> _units = new List<Unit>(10);
+        private readonly List<Mine> _mines = new List<Mine>(10);
+        private readonly List<Forest> _forest = new List<Forest>(10);
+        private readonly Market _market;
+        private readonly List<CenteredSprite> _sprites = new List<CenteredSprite>(100);
+
+        public Controller()
+        {
+            _units.Add(new Unit(new Point(200, 200), 4, 20, _sprites));
+            _units.Add(new Unit(new Point(100, 100), 3, 20, _sprites));
+            _units.Add(new Unit(new Point(300, 300), 3, 20, _sprites));
+
+            _mines.Add(new Mine(100, new Point(400, 120), 1, false, _sprites));
+            _mines.Add(new Mine(75, new Point(200, 600), 1, true, _sprites));
+
+            _forest.Add(new Forest(new Rect(700, 200, 300, 100)));
+
+            _market = new Market(new Rect(600, 500, 128, 128));
+
+            _sprites.Add(_units[0]);
+            _sprites.Add(_units[1]);
+            _sprites.Add(_units[2]);
+            _sprites.Add(_mines[0]);
+            _sprites.Add(_mines[1]);
+        }
+
+        public IEnumerable<UIElement> GetVisualSprites()
+        {
+            foreach (var unit in _units)
+                yield return unit.Visual;
+            foreach (var forest in _forest)
+                yield return forest.Visual;
+            foreach (var mine in _mines)
+                yield return mine.Visual;
+            yield return _market.Visual;
+        }
+
+        public IEnumerable<Action> CheckForMovement()
+        {
+            foreach (var unit in _units)
+            {
+                if (unit.CheckForMovement())
+                    yield return new Action(() => unit.RefreshPosition());
+            }
+        }
+
+        public void ClearFocus()
+        {
+            _units.ForEach(x => x.ChangeFocus(false, false));
+        }
+
+        public void FocusOnZone(Rect zone)
+        {
+            _units.ForEach(x =>
+            {
+                if (zone.Contains(x.Position))
+                    x.ChangeFocus(true, false);
+            });
+        }
+
+        public void RefreshHover(Rect zone)
+        {
+            _units.ForEach(x => x.RefreshVisual(zone.Contains(x.Position)));
+        }
+
+        public void SetTargetPositionsOnFocused(Point clickPosition)
+        {
+            var mine = _mines.FirstOrDefault(x => x.Surface.Contains(clickPosition));
+            if (mine != null)
+                clickPosition = mine.Position;
+            else if (_market.Surface.Contains(clickPosition))
+                clickPosition = _market.Position;
+
+            foreach (var unit in _units.Where(x => x.Focused))
+                unit.TargetPosition = clickPosition;
+        }
+    }
+}
--- Age Of Nothing/MainWindow.xaml.cs
+++ Age Of Nothing/MainWindow.xaml.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Timers;
+﻿using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
-using Age_Of_Nothing.Sprites;
 
 namespace Age_Of_Nothing
 {
@@ -16,19 +12,12 @@ namespace Age_Of_Nothing
     public partial class MainWindow : Window
     {
         private const int Fps = 20;
-
         private static readonly double Delay = 1 / (Fps / (double)1000);
-
         private readonly Timer _timer = new Timer(Delay);
-        private readonly List<Unit> _units = new List<Unit>(10);
-        private readonly List<Mine> _mines = new List<Mine>(10);
-        private readonly List<Forest> _forest = new List<Forest>(10);
-        private readonly Market _market;
-        private readonly List<CenteredSprite> _sprites = new List<CenteredSprite>(100);
         private readonly Rectangle _selectionRectGu;
-
         private Point? _selectionPoint;
         private volatile bool _refreshing = false;
+        private readonly Controller _controller;
 
         public MainWindow()
         {
@@ -36,33 +25,16 @@ namespace Age_Of_Nothing
             _timer.Elapsed += Refresh;
             _timer.Start();
 
-            _units.Add(new Unit(new Point(200, 200), 4, 20, _sprites));
-            _units.Add(new Unit(new Point(100, 100), 3, 20, _sprites));
-            _units.Add(new Unit(new Point(300, 300), 3, 20, _sprites));
-
             _selectionRectGu = new Rectangle
             {
                 Fill = Brushes.Red,
                 Opacity = 0.1
             };
 
-            _mines.Add(new Mine(100, new Point(400, 120), 1, false, _sprites));
-            _mines.Add(new Mine(75, new Point(200, 600), 1, true, _sprites));
+            _controller = new Controller();
 
-            _forest.Add(new Forest(new Rect(700, 200, 300, 100)));
-
-            _market = new Market(new Rect(600, 500, 128, 128));
-
-            _sprites.Add(_units[0]);
-            _sprites.Add(_units[1]);
-            _sprites.Add(_units[2]);
-            _sprites.Add(_mines[0]);
-            _sprites.Add(_mines[1]);
-
-            foreach (var sprite in _sprites)
-                MainCanvas.Children.Add(sprite.Visual);
-            MainCanvas.Children.Add(_forest[0].Visual);
-            MainCanvas.Children.Add(_market.Visual);
+            foreach (var vs in _controller.GetVisualSprites())
+                MainCanvas.Children.Add(vs);
             MainCanvas.Children.Add(_selectionRectGu);
         }
 
@@ -71,11 +43,8 @@ namespace Age_Of_Nothing
             if (_refreshing) return;
             _refreshing = true;
 
-            foreach (var unit in _units)
-            {
-                if (unit.CheckForMovement())
-                    Dispatcher.BeginInvoke(new Action(() => unit.RefreshPosition()));
-            }
+            foreach (var move in _controller.CheckForMovement())
+                Dispatcher.BeginInvoke(move);
 
             _refreshing = false;
         }
@@ -84,18 +53,7 @@ namespace Age_Of_Nothing
 
         private void MainCanvas_MouseRightButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            var clickPosition = e.GetPosition(MainCanvas);
-
-            var mine = _mines.FirstOrDefault(x => x.Surface.Contains(clickPosition));
-            if (mine != null)
-                clickPosition = mine.Position;
-            else if (_market.Surface.Contains(clickPosition))
-                clickPosition = _market.Position;
-
-            foreach (var unit in _units.Where(x => x.Focused))
-            {
-                unit.TargetPosition = clickPosition;
-            }
+            _controller.SetTargetPositionsOnFocused(e.GetPosition(MainCanvas));
         }
 
         private void MainCanvas_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -103,22 +61,14 @@ namespace Age_Of_Nothing
             if (sender == e.Source)
             {
                 _selectionPoint = e.GetPosition(MainCanvas);
-                _units.ForEach(x => x.ChangeFocus(false, false));
+                _controller.ClearFocus();
             }
         }
 
         private void MainCanvas_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            var endSelectionPoint = e.GetPosition(MainCanvas);
             if (_selectionPoint.HasValue)
-            {
-                var rect = new Rect(endSelectionPoint, _selectionPoint.Value);
-                _units.ForEach(x =>
-                {
-                    if (rect.Contains(x.Position))
-                        x.ChangeFocus(true, false);
-                });
-            }
+                _controller.FocusOnZone(new Rect(e.GetPosition(MainCanvas), _selectionPoint.Value));
             ResetSelectionRectangle();
         }
 
@@ -136,7 +86,7 @@ namespace Age_Of_Nothing
                 _selectionRectGu.Height = rect.Height;
                 _selectionRectGu.SetValue(Canvas.LeftProperty, rect.Left);
                 _selectionRectGu.SetValue(Canvas.TopProperty, rect.Top);
-                _units.ForEach(x => x.RefreshVisual(rect.Contains(x.Position)));
+                _controller.RefreshHover(rect);
             }
         }
 
